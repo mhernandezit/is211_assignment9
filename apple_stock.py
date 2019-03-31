e5ab614d83c1fe08c6d859d61ec1217472022feb
mhernandezit <mhernandezit@gmail.com>
2019-03-31 19:18:36 -0400

    Code refactor, linting

--- apple_stock.py
+++ apple_stock.py
@@ -1,36 +1,51 @@
+""" Apple_Stock module - pulls and parses stock data from Nasdaq.com """
+from urllib2 import Request, urlopen, URLError, HTTPError
 from bs4 import BeautifulSoup
-import pprint
-import urllib.request
 
 URL = 'https://www.nasdaq.com/symbol/aapl/historical'
 
-with urllib.request.urlopen(URL) as data:
-    pageData = BeautifulSoup(data, features="lxml")
-
-appleStockData = {}
-
-stockData = pageData.find("div", class_="genTable")
-tableData = stockData.find_all("tr")
-
-for row in tableData:
-    table = row.find_all('td')
+def download_data(url):
+    """Download the CSV at the url provided, return a URLlib response object"""
     try:
-        date = table[0].get_text().strip()
-        if date is '':
-            continue
-        appleStockData[date] = {}
-        appleStockData[date]['Closing'] = table[4].get_text().strip()
-    except IndexError:
+        req = Request(url)
+        response = urlopen(req)
+    except HTTPError:
         pass
-"""
-for table in stockData:
-    rows = table.find_all('tr')
-    for row in rows:
-        tableData = row.find_all('td')
-        date = tableData[0].get_text().strip()
-        if date is '':
+    except URLError:
+        pass
+    return response
+
+def process_data(data):
+    """ Convert the data into a beautifulsoup object, and parse the object
+    for table data.
+    We will be returning a dictionary with the date, closing price data """
+    soup = BeautifulSoup(data, features="lxml")
+
+    stock_data = {}
+
+    table_data = soup.find("div", class_="genTable")
+    row_data = table_data.find_all("tr")
+    for row in row_data:
+        column = row.find_all('td')
+        if not column:
             continue
-        appleStockData[date] = {}
-        appleStockData[date]['Closing'] = tableData[4].get_text().strip()
-"""
-pprint.pprint(appleStockData)
+        if column[0].get_text() == '\n':
+            continue
+        date = column[0].get_text().strip()
+        stock_data[date] = {}
+        stock_data[date]['Closing'] = column[4].get_text().strip()
+    return stock_data
+
+def print_data(dict_data):
+    """ Prints out the stock data """
+    print("{:10} : {:7}").format('Date', 'Closing Price')
+    for date, statistics in dict_data.items():
+        print("{:10} : {:7}").format(date, statistics['Closing'])
+
+def main():
+    """ Main function - nested functions to load the data from the CSV,
+    process the data, and finally print the processed data """
+    print_data(process_data(download_data(URL)))
+
+if __name__ == "__main__":
+    main()
--- football_stats.py
+++ football_stats.py
@@ -1,21 +1,47 @@
+""" Football Stats module - pulls and parses data from cbssports.com """
+from urllib2 import Request, urlopen, URLError, HTTPError
 from bs4 import BeautifulSoup
-import pprint
-import urllib.request
 
-URL = "https://www.cbssports.com/nfl/stats/playersort/nfl/year-2018-season-regular-category-touchdowns"
+URL = ("https://www.cbssports.com/nfl/stats/"
+       "playersort/nfl/year-2018-season-regular-category-touchdowns")
 
-with urllib.request.urlopen(URL) as data:
-    tdData = BeautifulSoup(data, features="lxml")
+def download_data(url):
+    """Download the CSV at the url provided, return a URLlib response object"""
+    try:
+        req = Request(url)
+        response = urlopen(req)
+    except HTTPError:
+        pass
+    except URLError:
+        pass
+    return response
 
-playerData = tdData.find_all("tr", class_={"row1","row2"})
+def process_data(data):
+    """ Convert the data into a beautifulsoup object, and parse the object
+    for table data.
+    We will be returning a dictionary with the player name as the key, and
+    the Position, Team and TD data as values """
+    td_data = BeautifulSoup(data, features="lxml")
+    player_data = td_data.find_all("tr", class_={"row1", "row2"})
+    top20_data = {}
+    for player in player_data[:20]:
+        player_name = player.contents[0].get_text()
+        top20_data[player_name] = {}
+        top20_data[player_name]['Position'] = player.contents[1].get_text()
+        top20_data[player_name]['Team'] = player.contents[2].get_text()
+        top20_data[player_name]['TD'] = player.contents[6].get_text()
+    return top20_data
 
-top20Data = {}
+def print_data(dict_data):
+    """ Print function to retrieve and print out the dictionary items """
+    print('{:20} : {:8} : {:6} : {:6}').format('Player', 'Position', 'Team', 'TD')
+    for player, stats in dict_data.items():
+        print('{:20} : {:8} : {:6} : {:6}').format(player, stats['Position'],
+                                                   stats['Team'], stats['TD'])
 
-for player in playerData[:20]:
-    playerName = player.contents[0].get_text()
-    top20Data[playerName] = {}
-    top20Data[playerName]['Position'] = player.contents[1].get_text()
-    top20Data[playerName]['Team'] = player.contents[2].get_text()
-    top20Data[playerName]['Total Touchdowns'] = player.contents[6].get_text()
+def main():
+    """ Main function - nested to download, process and display the data """
+    print_data(process_data(download_data(URL)))
 
-pprint.pprint(top20Data, width=30)
+if __name__ == "__main__":
+    main()
